6aa1e687274942a718a05bda55e31eb96982aacb
Nahin <nahin.ahmed28@gmail.com>
2022-11-29 19:13:08 +0600

    db updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,6 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\Category;
-use App\Models\Company;
 use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -21,44 +19,6 @@ class DatabaseSeeder extends Seeder
     {
         $faker = \Faker\Factory::create();
 
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
-//        for($i=0; $i<=30; $i++):
-//            DB::table('categories')
-//                ->insert([
-//                    'name' => $faker->name,
-//                    'created_at'=>	now(),
-//                    'updated_at'=>now(),
-//                ]);
-//        endfor;
-//
-//        for($i=0; $i<=15; $i++):
-//            DB::table('companies')
-//                ->insert([
-//                    'title' =>$faker->company,
-//                    'created_at'=>	now(),
-//                    'updated_at'=>now(),
-//                ]);
-//        endfor;
-//
-//        for($i=0; $i<=100; $i++):
-//            DB::table('products')
-//                ->insert([
-//                    'name' => $faker->name,
-//                    'description' => $faker->sentence(20),
-//                    'company_id' => Company::all()->random()->id,
-//                    'category_id' => Category::all()->random()->id,
-//                    'price' => $faker->randomFloat(2, 0, 10000),
-//                    'expire_date' =>  $faker->dateTimeBetween('-2 years')->format('Y-m-d'),
-//                    'created_at'=>	now(),
-//                    'updated_at'=>now(),
-//                ]);
-//        endfor;
 
         DB::table('users')->insert([
             'name' => 'admin',
@@ -77,16 +37,6 @@ class DatabaseSeeder extends Seeder
             'updated_at'=>now(),
         ]);
 
-//        DB::table('categories')->insert([
-//
-//            'name' => "offer",
-//            'company' => 'abcd efg hij ',
-//            'start_date' =>  Carbon::parse('2000-01-01'),
-//            'end_date' =>  Carbon::parse('2000-01-01'),
-//            'created_at'=>	now(),
-//            'updated_at'=>now(),
-//        ]);
-
 
         DB::table('abouts')->insert([
             'description' => 'description description description description',
@@ -95,6 +45,13 @@ class DatabaseSeeder extends Seeder
         ]);
 
         DB::table('missions')->insert([
+            'title' => 'title',
+            'description' => 'description description description description',
+            'image' => NULL,
+            'created_at'=>	now(),
+            'updated_at'=>now(),
+        ]);
+        DB::table('services')->insert([
             'title' => 'title',
             'description' => 'description description description description',
             'image' => NULL,
