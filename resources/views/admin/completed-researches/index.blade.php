bb90c56c533a328079d5c3faea8a31ca51a9f12c
riffAt2013 <rifat.masud@northsouth.edu>
2022-12-01 17:18:10 +0600

    remaining edit layout fixes

--- resources/views/admin/completed-researches/index.blade.php
+++ resources/views/admin/completed-researches/index.blade.php
@@ -5,33 +5,33 @@
 
 <div class="main-card mb-3 card">
     <div class="card-body table-full-width table-responsive">
-    <h4> <strong class="text-bg-primary">Completed Research LIST</strong></h4>
-    <table class="table table-hover table-striped">
-        <thead class="badge-light">
-        <th>ID</th>
-        <th>Title</th>
-        <th>Description</th>
-        <th>Action </th>
+        <h4> Completed Research LIST</h4>
+        <table class="table table-hover table-striped">
+            <thead class="badge-light">
+                <th>ID</th>
+                <th>Title</th>
+                <th>Description</th>
+                <th>Action </th>
 
-        </thead>
-        <tbody>
-                    @foreach($rows as $value)
-                        <tr>
-                            <td>{{$value->id}}</td>
-                            <td>{{$value->title}}</td>
-                            <td>{{$value->description}}</td>
-                            <td>
-                                <a href="{{ route('completed-research.edit',[$value->id]) }}" title="Edit">
-                                    <button class="btn btn-outline-primary btn-sm"><i class="fa fa-edit" aria-hidden="true"></i>
-                                    </button></a>
-                            </td>
-                        </tr>
-                    @endforeach
-        </tbody>
-    </table>
-    <div class="d-flex justify-content-center">
-        {!! $rows->links() !!}
+            </thead>
+            <tbody>
+                @foreach($rows as $value)
+                <tr>
+                    <td>{{$value->id}}</td>
+                    <td>{{$value->title}}</td>
+                    <td>{{$value->description}}</td>
+                    <td>
+                        <a href="{{ route('completed-research.edit',[$value->id]) }}" title="Edit">
+                            <button class="btn btn-outline-primary btn-sm"><i class="fa fa-edit" aria-hidden="true"></i>
+                            </button></a>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+        <div class="d-flex justify-content-center">
+            {!! $rows->links() !!}
+        </div>
     </div>
-</div>
 </div>
 @endsection
--- resources/views/admin/services/edit.blade.php
+++ resources/views/admin/services/edit.blade.php
@@ -2,58 +2,65 @@
 
 @section('content')
 
-    <div class="main-card mb-3 card">
-        <div class="card-body table-full-width table-responsive">
-            <h4> <strong class="text-bg-primary">Service Details Edit</strong></h4>
-            <!--begin::Form-->
-            <form  action="{{route('service.update', $service->id)}}" method="post" enctype="multipart/form-data">
-                @csrf
-                @method('PUT')
-                <div class="row">
-                    <div class="row col-md-6">
+<div class="main-card mb-3 card">
+    <div class="card-body table-full-width table-responsive">
+        <h4> Service Details Edit</h4>
+        <!--begin::Form-->
+        <form action="{{route('service.update', $service->id)}}" method="post" enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
+            <div class="row">
+                <div class="row col-md-6">
 
-                        <div class="col-xs-12 col-sm-12">
-                            <div class="form-group  m-form__group {{ $errors->has('title') ? 'has-danger' : '' }}">
-                                <label class="form-control-label"><span class="text-danger">*</span> Titile </label>
-                                <textarea class="form-control" id="title" placeholder="" rows="3" name="title" cols="50">{{ old('title', $service->title) }}</textarea>
-                                @if ($errors->has('title'))
-                                    <div class="form-control-feedback">{{ $errors->first('title') }}</div>
-                                @endif
-                            </div>
+                    <div class="col-xs-12 col-sm-12">
+                        <div class="form-group  m-form__group {{ $errors->has('title') ? 'has-danger' : '' }}">
+                            <label class="form-control-label"><span class="text-danger">*</span> Titile </label>
+                            <textarea class="form-control" id="title" placeholder="" rows="3" name="title" cols="50">{{
+                                old('title', $service->title) }}</textarea>
+                            @if ($errors->has('title'))
+                            <div class="form-control-feedback">{{ $errors->first('title') }}</div>
+                            @endif
                         </div>
-                        <div class="col-xs-12 col-sm-12">
-                            <div class="form-group  m-form__group {{ $errors->has('description') ? 'has-danger' : '' }}">
-                                <label class="form-control-label"><span class="text-danger">*</span> Description </label>
-                                <textarea class="form-control" id="description" placeholder="" rows="3" name="description" cols="50">{{ old('description', $service->description) }}</textarea>
-                                @if ($errors->has('description'))
-                                    <div class="form-control-feedback">{{ $errors->first('description') }}</div>
-                                @endif
-                            </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12">
+                        <div class="form-group  m-form__group {{ $errors->has('description') ? 'has-danger' : '' }}">
+                            <label class="form-control-label"><span class="text-danger">*</span> Description </label>
+                            <textarea class="form-control" id="description" placeholder="" rows="3" name="description"
+                                cols="50">{{ old('description', $service->description) }}</textarea>
+                            @if ($errors->has('description'))
+                            <div class="form-control-feedback">{{ $errors->first('description') }}</div>
+                            @endif
                         </div>
-                        <div class="col-xs-12 col-sm-12">
-                            <div class="form-group">
-                                <label class="form-control-label">Service Image</label>
-                                <img class="img-thumbnail" src="{{ asset('assets/uploads/service/'.$service->image)}}" width="200px">
-                                <div class="custom-file">
-                                    <input type="file" name="image" class="custom-file-input form-control {{ $errors->has('image') ? 'is-invalid' : '' }}" id="PartnersImageFile" />
-                                    <label class="custom-file-label" for="PartnersImageFile">Choose file</label>
-                                    @if ($errors->has('image'))
-                                        <div class="invalid-feedback">{{ $errors->first('image') }}</div>
-                                    @endif
+                    </div>
+                    <div class="col-xs-12 col-sm-12">
+                        <div class="form-group">
+                            <label class="form-control-label">Service Image</label>
+                            <img class="img-thumbnail" src="{{ asset('assets/uploads/service/'.$service->image)}}"
+                                width="200px">
+                            <div class="custom-file">
+                                <input type="file" name="image"
+                                    class="custom-file-input form-control {{ $errors->has('image') ? 'is-invalid' : '' }}"
+                                    id="PartnersImageFile" />
+                                <label class="custom-file-label" for="PartnersImageFile">Choose file</label>
+                                @if ($errors->has('image'))
+                                <div class="invalid-feedback">{{ $errors->first('image') }}</div>
+                                @endif
 
-                                </div>
-                            </div>
-                        </div>
-                        <div class="m-portlet__foot m-portlet__foot--fit">
-                            <div class="m-form__actions text-center">
-                                <a href="{{ route('service.index') }}" class="btn btn-outline-warning"><i class="fa fa-times"></i> Cancel</a>
-                                <button type="submit" class="btn btn-success"><i class="fa fa-save"></i> Save</button>
                             </div>
                         </div>
                     </div>
+                    <div class="m-portlet__foot m-portlet__foot--fit">
+                        <div class="m-form__actions text-center flexbox">
+                            <a href="{{ route('service.index') }}" class="btn btn-danger btn-flex"><i
+                                    class="fa fa-times"></i> Cancel</a>
+                            <button type="submit" class="btn btn-success btn-flex"><i class="fa fa-save"></i>
+                                Save</button>
+                        </div>
+                    </div>
                 </div>
-            </form>
-            <!--end::Form-->
-        </div>
+            </div>
+        </form>
+        <!--end::Form-->
     </div>
+</div>
 @endsection
