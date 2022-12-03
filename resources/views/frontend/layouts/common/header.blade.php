ad32fc14c4eeb023532b5858c8e07278b09f9f32
nahin1 <nahin@mcc.com.bd>
2022-12-03 16:38:12 +0600

    navbar frontend mod

--- resources/views/frontend/layouts/common/header.blade.php
+++ resources/views/frontend/layouts/common/header.blade.php
@@ -1,9 +1,9 @@
 <header id="header" class="fixed-top ">
     <div class="container d-flex align-items-center justify-content-between">
 
-        <h1 class="logo"><a href="#">Public Side</a></h1>
+{{--        <h1 class="logo"><a href="#">Public Side</a></h1>--}}
         <!-- Uncomment below if you prefer to use an image logo -->
-        <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+     <a href="#" class="logo"><img src="{{asset('frontend/assets/img/logo.png')}}" alt="" class="img-fluid"></a>
 
         <nav id="navbar" class="navbar">
 
@@ -11,25 +11,38 @@
                 <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
                 <li><a class="nav-link scrollto" href="#about">About</a></li>
                 <li><a class="nav-link scrollto" href="#services">Services</a></li>
-                <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li>
-                <li><a class="nav-link scrollto" href="#team">Team</a></li>
-                <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+                <li class="dropdown"><a href="#"><span>Approach</span> <i class="bi bi-chevron-down"></i></a>
                     <ul>
-                        <li><a href="#">Drop Down 1</a></li>
-                        <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-                            <ul>
-                                <li><a href="#">Deep Drop Down 1</a></li>
-                                <li><a href="#">Deep Drop Down 2</a></li>
-                                <li><a href="#">Deep Drop Down 3</a></li>
-                                <li><a href="#">Deep Drop Down 4</a></li>
-                                <li><a href="#">Deep Drop Down 5</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#">Drop Down 2</a></li>
-                        <li><a href="#">Drop Down 3</a></li>
-                        <li><a href="#">Drop Down 4</a></li>
+                        <li><a href="#">Mission And Vision</a></li>
                     </ul>
                 </li>
+                <li><a class="nav-link scrollto" href="#team">Expertise</a></li>
+                <li class="dropdown"><a href="#"><span>Academic</span> <i class="bi bi-chevron-down"></i></a>
+                    <ul>
+                        <li><a href="#">Higher Education</a></li>
+{{--                        <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>--}}
+{{--                            <ul>--}}
+{{--                                <li><a href="#">Deep Drop Down 1</a></li>--}}
+{{--                                <li><a href="#">Deep Drop Down 2</a></li>--}}
+{{--                                <li><a href="#">Deep Drop Down 3</a></li>--}}
+{{--                                <li><a href="#">Deep Drop Down 4</a></li>--}}
+{{--                                <li><a href="#">Deep Drop Down 5</a></li>--}}
+{{--                            </ul>--}}
+{{--                        </li>--}}
+                        <li><a href="#">Training</a></li>
+                        <li><a href="#">Internship</a></li>
+                        <li><a href="#">Fellowship</a></li>
+                    </ul>
+                </li>
+                <li><a class="nav-link scrollto" href="#contact">Organization</a></li>
+                <li class="dropdown"><a href="#"><span>Research</span> <i class="bi bi-chevron-down"></i></a>
+                    <ul>
+                        <li><a href="#">Ongoing Research</a></li>
+                        <li><a href="#">Completed Research</a></li>
+                    </ul>
+                </li>
+
+                <li><a class="nav-link scrollto" href="#contact">Publication</a></li>
                 <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
                 @if (Route::has('login'))
                     <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
