fde73c1d91490599b24a401974df7b4fefef3b6f
NahinAhmed28 <Nahin.ahmed28@gmail.com>
2022-12-08 12:22:19 +0600

    front end data fetch continue

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -3,7 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Models\About;
+use App\Models\CompletedResearch;
+use App\Models\Contact;
+use App\Models\Expertise;
+use App\Models\Fellowship;
+use App\Models\Gallery;
+use App\Models\HigherEducation;
+use App\Models\Internship;
+use App\Models\Mission;
+use App\Models\OngoingResearch;
 use App\Models\Organization;
+use App\Models\Publication;
+use App\Models\Service;
+use App\Models\Training;
+use App\Models\Vision;
 use Illuminate\Http\Request;
 
 class FrontEndController extends Controller
@@ -27,52 +40,93 @@ class FrontEndController extends Controller
     }
     public function service()
     {
-        return view('frontend.layouts.service');
+        $data = [
+            'services' => Service::get(),
+        ];
+        return view('frontend.layouts.service', $data);
+
 
     }
     public function expertise()
     {
-        return view('frontend.layouts.expertise');
+        $data = [
+            'expertises' => Expertise::get(),
+        ];
+        return view('frontend.layouts.expertise', $data);
+
 
     }
     public function mission()
     {
-        return view('frontend.layouts.mission');
+        $data = [
+            'mission' => Mission::first(),
+            'vision' => Vision::first(),
+        ];
+        return view('frontend.layouts.mission', $data);
+
 
     }
     public function contact()
     {
-        return view('frontend.layouts.contact');
+        $data = [
+            'contact' => Contact::get(),
+        ];
+        return view('frontend.layouts.contact', $data);
+
 
     }
     public function higherEducation()
     {
-        return view('frontend.layouts.higher-education');
+        $data = [
+            'contact' => HigherEducation::get(),
+        ];
+        return view('frontend.layouts.higher-education', $data);
+
 
     }
     public function internship()
     {
-        return view('frontend.layouts.internship');
+        $data = [
+            'internships' => Internship::get(),
+        ];
+        return view('frontend.layouts.internship', $data);
+
 
     }
     public function training()
     {
-        return view('frontend.layouts.training');
+        $data = [
+            'trainings' => Training::get(),
+        ];
+        return view('frontend.layouts.training', $data);
+
 
     }
     public function fellowship()
     {
-        return view('frontend.layouts.fellowship');
+        $data = [
+            'internships' => Fellowship::get(),
+        ];
+        return view('frontend.layouts.fellowship', $data);
+
 
     }
     public function completedResearch()
     {
-        return view('frontend.layouts.completed-research');
+        $data = [
+            'internships' => CompletedResearch::get(),
+        ];
+        return view('frontend.layouts.completed-research', $data);
+
 
     }
     public function ongoingResearch()
     {
-        return view('frontend.layouts.ongoing-research');
+        $data = [
+            'internships' => OngoingResearch::get(),
+        ];
+        return view('frontend.layouts.ongoing-research', $data);
+
 
     }
     public function organization()
@@ -86,12 +140,20 @@ class FrontEndController extends Controller
     }
     public function publication()
     {
-        return view('frontend.layouts.publication');
+        $data = [
+            'internships' => Publication::get(),
+        ];
+        return view('frontend.layouts.publication', $data);
+
 
     }
     public function gallery()
     {
-        return view('frontend.layouts.gallery');
+        $data = [
+            'gallerys' => Gallery::get(),
+        ];
+        return view('frontend.layouts.gallery', $data);
+
 
     }
 
