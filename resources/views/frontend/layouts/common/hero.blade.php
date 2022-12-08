4053e3042139a7002d190046e26b8490d0e84fd3
NahinAhmed28 <Nahin.ahmed28@gmail.com>
2022-12-08 12:37:24 +0600

    front end data fetch continue

--- resources/views/frontend/layouts/common/hero.blade.php
+++ resources/views/frontend/layouts/common/hero.blade.php
@@ -1,6 +1,7 @@
 <section id="hero">
     <div class="hero-container" data-aos="fade-up" data-aos-delay="150">
         <h1>Welcome</h1>
+        <h1>This Website is Under Construction</h1>
         <h2>In order to see results</h2>
         <div class="d-flex">
             <a href="#team" class="btn-get-started scrollto">See Below</a>
