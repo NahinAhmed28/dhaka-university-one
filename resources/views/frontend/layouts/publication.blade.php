1c3400ecf686fc02ddd97cab2e9eeb333e51247c
riffAt2013 <rifat.masud@northsouth.edu>
2022-12-12 12:08:21 +0600

    data fetch complete except some

--- resources/views/frontend/layouts/publication.blade.php
+++ resources/views/frontend/layouts/publication.blade.php
@@ -2,87 +2,48 @@
 
 @section('content')
 
-    @push('style')
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
-    @endpush
+@push('style')
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+@endpush
 
-    <!-- ======= Hero Section ======= -->
-    @include('frontend.layouts.common.heroheader')
-    <!-- End Hero -->
+<!-- ======= Hero Section ======= -->
+@include('frontend.layouts.common.heroheader')
+<!-- End Hero -->
 
-    <!-- Start #main -->
-    <main id="main">
+<!-- Start #main -->
+<main id="main">
 
 
-        <!-- ======= Services Section ======= -->
-        <section id="services" class="services section-bg">
-            <div class="container" data-aos="fade-up">
-                <div class="section-title">
-                    <h2>Research</h2>
-                    <p>Publication</p>
-                </div>
-
-                <div class="row" data-aos="fade-up" data-aos-delay="200">
-                    <div class="col-md-6">
-                        <div class="icon-box">
-                            <i class="bi bi-laptop"></i>
-                            <h4><a href="#">Lorem Ipsum</a></h4>
-                            <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati
-                                cupiditate non provident</p>
-                        </div>
-                    </div>
-                    <div class="col-md-6 mt-4 mt-md-0">
-                        <div class="icon-box">
-                            <i class="bi bi-bar-chart"></i>
-                            <h4><a href="#">Dolor Sitema</a></h4>
-                            <p>Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                consequat tarad limino ata</p>
-                        </div>
-                    </div>
-                    <div class="col-md-6 mt-4 mt-md-0">
-                        <div class="icon-box">
-                            <i class="bi bi-brightness-high"></i>
-                            <h4><a href="#">Sed ut perspiciatis</a></h4>
-                            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                                pariatur</p>
-                        </div>
-                    </div>
-                    <div class="col-md-6 mt-4 mt-md-0">
-                        <div class="icon-box">
-                            <i class="bi bi-briefcase"></i>
-                            <h4><a href="#">Nemo Enim</a></h4>
-                            <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                                anim id est laborum</p>
-                        </div>
-                    </div>
-                    <div class="col-md-6 mt-4 mt-md-0">
-                        <div class="icon-box">
-                            <i class="bi bi-card-checklist"></i>
-                            <h4><a href="#">Magni Dolore</a></h4>
-                            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
-                                voluptatum deleniti atque</p>
-                        </div>
-                    </div>
-                    <div class="col-md-6 mt-4 mt-md-0">
-                        <div class="icon-box">
-                            <i class="bi bi-clock"></i>
-                            <h4><a href="#">Eiusmod Tempor</a></h4>
-                            <p>Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta
-                                nobis est eligendi</p>
-                        </div>
-                    </div>
-                </div>
-
+    <!-- ======= Services Section ======= -->
+    <section id="services" class="services section-bg">
+        <div class="container" data-aos="fade-up">
+            <div class="section-title">
+                <h2>Research</h2>
+                <p>Publication</p>
             </div>
-        </section><!-- End Services Section -->
+
+            <div class="row" data-aos="fade-up" data-aos-delay="200">
+                @foreach($publications as $publication)
+                <div class="col-md-6 mt-4 mt-md-0">
+                    <div class="icon-box">
+                        <i class="bi bi-clock"></i>
+                        <h4><a href="#">{{$publication->title}}</a></h4>
+                        <p>{!!$publication->description!!}</p>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+
+        </div>
+    </section><!-- End Services Section -->
 
 
-    </main><!-- End #main -->
+</main><!-- End #main -->
 
-    @push('scripts')
-        <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
-        <script>
-            flatpickr("input[type=datetime-local]");
-        </script>
-    @endpush
+@push('scripts')
+<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+<script>
+    flatpickr("input[type=datetime-local]");
+</script>
+@endpush
 @endsection
