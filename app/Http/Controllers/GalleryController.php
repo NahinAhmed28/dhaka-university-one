9c444be3bd3829bc85f2b3e3b2d909b7ef4843fd
Nahin <nahin.ahmed28@gmail.com>
2023-01-06 22:56:31 +0600

    home slider module updated for multiple slider and gallary image size is resized

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -53,7 +53,7 @@ class GalleryController extends Controller
                 mkdir('assets/uploads/gallery', 0777, true);
             }
             $image->move('assets/uploads/gallery', $imageFileName);
-            Image::make('assets/uploads/gallery/'.$imageFileName)->resize(600,400)->save('assets/uploads/gallery/'.$imageFileName);
+            Image::make('assets/uploads/gallery/'.$imageFileName)->resize(1000,800)->save('assets/uploads/gallery/'.$imageFileName);
         } else {
             $imageFileName = 'default_logo.png';
         }
