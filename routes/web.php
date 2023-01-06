63cee4d4da4895673480423bf5691e4997b2e0f2
Nahin <nahin.ahmed28@gmail.com>
2023-01-07 01:53:55 +0600

    wip

--- routes/web.php
+++ routes/web.php
@@ -88,6 +88,7 @@ Route::get('/clear', function() {
     Artisan::call('config:clear');
     Artisan::call('config:cache');
     Artisan::call('view:clear');
+    Artisan::call('route:clear');
 
     return "Cleared!";
 
